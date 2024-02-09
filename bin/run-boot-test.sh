1b579354203a87cdc2e3244be2859391dcc05313
Marco van Zwetselaar <io@zwets.it>
2024-02-09 11:49:40 +0300

    Fix path, clean up properties

--- bin/run-boot-test.sh
+++ bin/run-boot-test.sh
@@ -3,10 +3,10 @@
 export LC_ALL="C"
 set -euo pipefail
 
-cd "$(dirname "$(realpath "$0")")/../.."
+cd "$(dirname "$(realpath "$0")")/.."
 
 [ -f config/live.vault ] || {
-    cp src/main/resources/test.vault config/live.vault
+    cp src/main/resources/builtin.vault config/live.vault
     keytool -genkeypair -keyalg RSA -keysize 4096 -validity 36500 -storepass 123456 -keystore config/live.vault -storetype PKCS12 -alias live -dname CN=live
 }
 
