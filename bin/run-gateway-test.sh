cc176945183de182632a25812fed7a08b39801b7
Marco van Zwetselaar <io@zwets.it>
2024-02-19 14:31:25 +0300

    Start work on 1.0.5-SNAPSHOT

--- bin/run-gateway-test.sh
+++ bin/run-gateway-test.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 cd "$(dirname "$(realpath "$0")")/.."
 
-VERSION=1.0.4
+VERSION=1.0.5-SNAPSHOT
 JAR="target/sms-gateway-${VERSION}.jar"
 
 exec java -jar "$JAR" --spring.profiles.active='test' "$@"
