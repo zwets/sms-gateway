a3ffd403dcc01a154a218d9cd09e08bd7deb7450
Marco van Zwetselaar <io@zwets.it>
2024-02-27 14:18:52 +0300

    Release 1.1.0 (new crypto)

--- bin/run-boot-test.sh
+++ bin/run-boot-test.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$(realpath "$0")")/.."
 
 [ -f config/live.vault ] || {
     cp src/main/resources/builtin.vault config/live.vault
-    keytool -genkeypair -keyalg RSA -keysize 4096 -validity 36500 -storepass 123456 -keystore config/live.vault -storetype PKCS12 -alias live -dname CN=live
+    keytool -genkeypair -keyalg RSA -keysize 2048 -validity 36500 -storepass 123456 -keystore config/live.vault -storetype PKCS12 -alias live -dname CN=live
 }
 
 export SPRING_PROFILES_ACTIVE=test
